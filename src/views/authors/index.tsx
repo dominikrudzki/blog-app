e05911b425cca6fb9669a7532e7e076ed2883ba3
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-03-08 15:38:26 +0100

    create list of authors in Authors

--- src/views/authors/index.tsx
+++ src/views/authors/index.tsx
@@ -1,7 +1,57 @@
 import { Headline } from "../../components/shared/headline"
+import { useEffect, useState } from "react"
+import { getBlogs } from "../../services/data.service"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+export const AuthorList = styled.ul`
+	display: flex;
+	gap: .2em;
+	flex-wrap: wrap;
+
+	li {
+		color: var(--font-white);
+		transition: background-color .3s;
+		padding: .5em 1em;
+		margin: .2em 0;
+		background-color: #7E7E7E16;
+		border-radius: 15px;
+
+		&:hover {
+			background-color: #3054a2;
+		}
+	}
+`
 
 const Authors = () => {
-	return <Headline>Authors</Headline>
+	const [authors, setAuthors] = useState<Array<string>>([])
+
+	useEffect(() => {
+		getBlogs().then(({data, success}) =>
+			success &&
+			setAuthors([...Array.from(new Set(data.map(blog =>
+				(blog.author))
+				.sort().filter(val =>
+					val !== '')))]
+			)
+		)
+	}, [])
+
+	return (
+		<>
+			<Headline>Authors</Headline>
+			<AuthorList>
+				{authors.map((author, index) =>
+					<Link
+						key={index}
+						to={{pathname: '/', search: `?author=${author}`}}
+					>
+						<li>{author}</li>
+					</Link>
+				)}
+			</AuthorList>
+		</>
+	)
 }
 
 export default Authors
